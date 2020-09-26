0e8b479d53ec4a1296069dc96e88816d2fe95305
Rodrigo Jacznik <rodrigo.jacznik@501labs.com.ar>
2020-09-26 16:33:13 -0300

    Prueba lint-staged

--- web/src/components/Logo/Logo.js
+++ web/src/components/Logo/Logo.js
@@ -1,13 +1,26 @@
 const Logo = (props) => {
   return (
-    <svg {...props} viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="50" height="50">
-        <circle cx="25" cy="25" r="25" fill="#C4C4C4"/>
+    <svg
+      {...props}
+      viewBox="0 0 50 50"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <mask
+        id="mask0"
+        mask-type="alpha"
+        maskUnits="userSpaceOnUse"
+        x="0"
+        y="0"
+        width="50"
+        height="50"
+      >
+        <circle cx="25" cy="25" r="25" fill="#C4C4C4" />
       </mask>
       <g mask="url(#mask0)">
-        <rect x="25" width="25" height="25" fill="#4C51BF"/>
-        <rect x="25" y="25" width="25" height="25" fill="#7F9CF5"/>
-        <rect y="25" width="25" height="25" fill="#EBF4FF"/>
+        <rect x="25" width="25" height="25" fill="#4C51BF" />
+        <rect x="25" y="25" width="25" height="25" fill="#7F9CF5" />
+        <rect y="25" width="25" height="25" fill="#EBF4FF" />
       </g>
     </svg>
   )
